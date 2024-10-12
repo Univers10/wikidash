24c73314fb39118161bcdb0d92a51aa3baf8ef1c
UNIVERS10 <joelattitso@gmail.com>
2024-10-12 03:04:45 +0000

    je ne sais pas quoi dire

---
+++ resources/views/dash/articles-modifier.blade.php
@@ -0,0 +1,73 @@
+@extends('dash.base')
+@section('content')
+@section('page', 'Mes articles modifiés')
+<div class="mb-40">
+    <div class="row gy-4">
+        <div class="col-xxl-3 col-sm-6">
+            <div class="card h-100 radius-12">
+                <img src="assets/images/card-component/card-img1.png" class="card-img-top" alt="">
+                <div class="card-body p-16">
+                    <h5 class="card-title text-lg text-primary-light
+                         mb-6">This is Card title
+                    </h5>
+                    <p class="card-text text-neutral-600">We quickly learn to fear and thus automatically avo id
+                        potentially stressful situations of all kinds, including the most common of all .</p>
+                    <a href="javascript:void(0)"
+                        class="btn text-primary-600 hover-text-primary px-0 py-10 d-inline-flex align-items-center gap-2">
+                        Read More <iconify-icon icon="iconamoon:arrow-right-2" class="text-xl"></iconify-icon>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-sm-6">
+            <div class="card h-100 radius-12">
+                <img src="assets/images/card-component/card-img1.png" class="card-img-top" alt="">
+                <div class="card-body p-16">
+                    <h5 class="card-title text-lg text-primary-light
+                         mb-6">This is Card title
+                    </h5>
+                    <p class="card-text text-neutral-600">We quickly learn to fear and thus automatically avo id
+                        potentially stressful situations of all kinds, including the most common of all .</p>
+                    <a href="javascript:void(0)"
+                        class="btn text-primary-600 hover-text-primary px-0 py-10 d-inline-flex align-items-center gap-2">
+                        Read More <iconify-icon icon="iconamoon:arrow-right-2" class="text-xl"></iconify-icon>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-sm-6">
+            <div class="card h-100 radius-12">
+                <img src="assets/images/card-component/card-img1.png" class="card-img-top" alt="">
+                <div class="card-body p-16">
+                    <h5 class="card-title text-lg text-primary-light
+                         mb-6">This is Card title
+                    </h5>
+                    <p class="card-text text-neutral-600">We quickly learn to fear and thus automatically avo id
+                        potentially stressful situations of all kinds, including the most common of all .</p>
+                    <a href="javascript:void(0)"
+                        class="btn text-primary-600 hover-text-primary px-0 py-10 d-inline-flex align-items-center gap-2">
+                        Read More <iconify-icon icon="iconamoon:arrow-right-2" class="text-xl"></iconify-icon>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-sm-6">
+            <div class="card h-100 radius-12">
+                <img src="assets/images/card-component/card-img1.png" class="card-img-top" alt="">
+                <div class="card-body p-16">
+                    <h5 class="card-title text-lg text-primary-light
+                         mb-6">This is Card title
+                    </h5>
+                    <p class="card-text text-neutral-600">We quickly learn to fear and thus automatically avo id
+                        potentially stressful situations of all kinds, including the most common of all .</p>
+                    <a href="javascript:void(0)"
+                        class="btn text-primary-600 hover-text-primary px-0 py-10 d-inline-flex align-items-center gap-2">
+                        Read More <iconify-icon icon="iconamoon:arrow-right-2" class="text-xl"></iconify-icon>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('dash.index');
+})->name('dash');
+
+Route::get('/articles-signales', function () {
+    return view('dash.articles-signales');
+})->name('articles-signales');
+
+Route::get('/articles-modifier', function () {
+    return view('dash.articles-modifier');
+})->name('articles-modifier');
+
+Route::get('/articles', function () {
+    return view('dash.articles');
+})->name('articles');
